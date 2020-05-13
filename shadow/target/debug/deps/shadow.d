2ed6c5b782f16a87f0feb6efb183bdee2f561bae
nomansadiq11 <nomi.cs786@gmail.com>
2020-05-13 21:36:18 +0400

    shadow variable learhing

---
+++ shadow/target/debug/deps/shadow.d
@@ -0,0 +1,5 @@
+G:\Documents\Projects\GIT\repo\rust-learning\shadow\target\debug\deps\shadow.exe: src\main.rs
+
+G:\Documents\Projects\GIT\repo\rust-learning\shadow\target\debug\deps\shadow.d: src\main.rs
+
+src\main.rs:
---
+++ shadow/target/debug/shadow.d
@@ -0,0 +1,1 @@
+G:\Documents\Projects\GIT\repo\rust-learning\shadow\target\debug\shadow.exe: G:\Documents\Projects\GIT\repo\rust-learning\shadow\src\main.rs
