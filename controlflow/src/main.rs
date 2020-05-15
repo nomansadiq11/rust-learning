986740929ffc34cf972e67d667b0adff51f78d55
nomansadiq11 <nomi.cs786@gmail.com>
2020-05-15 22:04:22 +0400

    if else learn

---
+++ controlflow/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    
+
+    let num = 0; 
+
+
+    if num < 0 
+    {
+        println!("number is negietive "); 
+    }
+    else if num == 0 
+    {
+        println!("number is zero"); 
+    }
+
+    else
+    {
+        println!("number is postitive"); 
+    }
+
+    let even = false; 
+
+    let num =  {
+
+        if even
+        {
+            6
+        }
+        else
+        {
+            7
+        }
+
+
+    };
+
+
+    println!("{}", num); 
+
+}
+
+
+
+
