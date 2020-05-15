cda391e5aaeef9d2f6d3ec18017a045517741a4a
nomansadiq11 <nomi.cs786@gmail.com>
2020-05-15 15:41:58 +0400

    function learn

---
+++ functions/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    
+
+    
+    let num = {
+        let i = 19; 
+        i +1
+    }; 
+    
+    
+    println!("{}", num); 
+
+}
+
+
+fn CakeMake()
+{
+    println!("1. Milk"); 
+    println!("1. Egg"); 
+}
+
+
+
+fn sequre(x:u32)
+{
+    let val = x * 2; 
+    println!("{}", val); 
+
+}
