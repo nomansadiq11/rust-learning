9689118642e38dbf4732119a11fa1859d9ed7058
nomansadiq11 <nomi.cs786@gmail.com>
2020-05-17 23:09:11 +0400

    learn function chages

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -7,8 +7,9 @@ fn main() {
         i +1
     }; 
     
+    let (val, val1) = sequre(2, 3.5); 
     
-    println!("{}", num); 
+    println!("{}, {}", val, val1); 
 
 }
 
@@ -21,9 +22,10 @@ fn CakeMake()
 
 
 
-fn sequre(x:u32)
+fn sequre(x:u32, y:f64) -> (u32, f64)
 {
-    let val = x * 2; 
-    println!("{}", val); 
+    let val = x * x; 
+    let val1 = y * y; 
+    (val, val1)
 
 }
